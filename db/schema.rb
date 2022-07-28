2d2141bcfcd6f62e31c47ba3f74881b5b43de428
bgdnvk <bnovykov@gmail.com>
2022-07-28 19:02:23 +0200

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_27_213234) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_28_165729) do
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
     t.string "string"
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_27_213234) do
     t.string "twitter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
